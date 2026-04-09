9d7c9c86b0850f400379bef1062559f055649bf8
LucasLessaAnacleto <lucaslessaanacleto@gmail.com>
2026-04-09 20:56:09 -0300

    Removendo metodo sem uso

--- backend/src/main/java/com/senac/ApiEduPlataformSenac/model/repository/ProfessorRepository.java
+++ backend/src/main/java/com/senac/ApiEduPlataformSenac/model/repository/ProfessorRepository.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
     Optional<Professor> findByEmail(String email);
-    Optional<Professor> findByIdAndEmail(Long id, String email);
 }
